9b99286e14497740f55c05b7d4c53ccc3cdc6e3e
Nicki Nixon <nicki.nixon@mx.com>
2021-11-12 10:38:05 -0700

    new/create actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,24 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio_items = Portfolio.all
     end
+
+    def new
+        @portfolio_item = Portfolio.new
+    end
+
+    def create
+        @portfolio_item = Portfolio.new(portfolio_params)
+        respond_to do |format|
+            if @portfolio_item.save
+                format.html { redirect_to @portfolio_item, notice: "Your portfolio entry was successfully saved."}
+            else
+                format.html { render :new, notice: "Error, entry was not saved."}
+            end
+        end
+    end
+
+    private
+        def portfolio_params
+        params.require(:portfolio).permit(:title, :subtitle, :body)
+        end
 end
